fdf64eb62f4771020b49a8455fc8e4bdd56c3639
squoop <the.sam.mcg@protonmail.com>
2023-06-06 18:54:23 +0100

    Refactoring.

--- src/recursive.rs
+++ src/recursive.rs
@@ -1,36 +1,32 @@
 use std::cell::OnceCell;
-use std::marker::PhantomData;
 use std::rc::Rc;
 
 use crate::context::Context;
 use crate::parser::Parser;
 
-// Method taken from https://crates.io/crates/chumsky.
-
-struct RecursiveInner<C: Context, Output> {
-    parser: OnceCell<Box<dyn Parser<C, Output>>>,
-    _phantom: PhantomData<*const (C, Output)>,
-}
+// Technique stolen from https://crates.io/crates/chumsky.
 
 pub struct Recursive<C: Context, Output> {
-    inner: Rc<RecursiveInner<C, Output>>,
+    parser: OnceCell<Box<dyn Parser<C, Output>>>,
 }
 
-impl<C: Context, Output> Clone for Recursive<C, Output> {
-    #[inline]
-    fn clone(&self) -> Self {
-        Self {
-            inner: self.inner.clone(),
+impl<C: Context, Output> Recursive<C, Output> {
+    fn declare() -> Rc<Self> {
+        Rc::new(Self {
+            parser: OnceCell::new(),
+        })
+    }
+
+    fn define(&self, parser: impl Parser<C, Output> + 'static) {
+        if self.parser.set(Box::new(parser)).is_err() {
+            panic!("Tried to define the parser multiple times.");
         }
     }
 }
 
-impl<C: Context, Output> Recursive<C, Output> {}
-
-impl<C: Context, Output> Parser<C, Output> for Recursive<C, Output> {
+impl<C: Context, Output> Parser<C, Output> for Rc<Recursive<C, Output>> {
     fn parse(&self, context: &mut C) -> Output {
         let parser = self
-            .inner
             .parser
             .get()
             .expect("parser called before (end of) definition");
@@ -38,21 +34,16 @@ impl<C: Context, Output> Parser<C, Output> for Recursive<C, Output> {
     }
 }
 
+/// Construct a parser that can call itself.
+///
+/// A reference to the parser itself is provided to the builder, and can be used
+/// to construct a recursive parser.
+///
+/// Panics if the parser is called inside the builder.
 pub fn recursive<C: Context, P: Parser<C, Output> + 'static, Output>(
-    get_parser: impl Fn(Recursive<C, Output>) -> P,
-) -> Recursive<C, Output> {
-    let inner = RecursiveInner {
-        parser: OnceCell::new(),
-        _phantom: PhantomData,
-    };
-    let recursive = Recursive {
-        inner: Rc::new(inner),
-    };
-
-    let parser = get_parser(recursive.clone());
-
-    // Can't fail but can't call unwrap since `P` doesn't implement `Debug`.
-    let _ = recursive.inner.parser.set(Box::new(parser));
-
-    recursive
+    build_parser: impl Fn(Rc<Recursive<C, Output>>) -> P,
+) -> Rc<Recursive<C, Output>> {
+    let rec = Recursive::declare();
+    rec.define(build_parser(rec.clone()));
+    rec
 }
