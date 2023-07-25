83231a562624eb59018fb87bab1741acafb9e013
squoop <the.sam.mcg@protonmail.com>
2023-07-25 17:50:44 +0100

    Fix `ident` generics.

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 use crate::context::slice::Slice;
 use crate::prelude::*;
 
-pub fn ident<In: Slice<Token = char>, Out, Error>() -> impl Parser<In, In, Error> {
+pub fn ident<In: Slice<Token = char>, Error>() -> impl Parser<In, In, Error> {
     chain((pred(is_ident_start), pred(is_ident).repeat())).to_slice()
 }
 
